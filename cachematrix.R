23be81a3bdbb0c72a6a4fc6743f2c395f070fb8d
flyrod1821 <flyrod1821@users.noreply.github.com>
2015-04-19 12:24:26 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script creates a matrix that is stored in cache memory. 
+## The second part of the script creates a function that calculates the inverse of the maxtrix.
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL             ##      Sets m to null if function has not been run
+  set <- function(y) {  ##      creates function set that assigns x and m values
+        x <<- y         ##      These variables are set to the parent environment in makeCacheMatrix                      
+        m <<- NULL                           
+                                         
+  }
+  get <- function() x                    
+    setinverse <- function(solve) m <<- solve  
+    getinverse <- function() m            
+                                             
+  list(set = set, get = get,           
+       setinverse = setinverse,         ##      Creates list of variable names and 
+       getinverse = getinverse)         ##      calculated values
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {        ##      Pulls the m value from the other function
+        m <- x$getinverse()            ##     environment to this environment         
+                                      
+        if(!is.null(m)) {               ##      Tests to see if function has been run previously 
+                                     
+        message("getting cached data")
+        return(m)
+  }
+        data <- x$get()                      
+                                     
+                                     
+        m <- solve(data, ...)              
+                                    
+        x$setinverse(m)                   
+                                     
+        m                            ##         returns the inverse of the matrix   
 }
+
